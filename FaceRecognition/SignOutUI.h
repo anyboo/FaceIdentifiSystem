611d7c6c567c9fe725be623ff4196d45814bd06b
wwang13 <191373193@qq.com>
2016-08-03 17:45:17 +0800

    SignOutUI.cpp and SignOutUI.h

---
+++ FaceRecognition/SignOutUI.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <DuiLib/UIlib.h>
+
+
+class CRegisterInfo;
+
+#define BT_CLOSE_MatchWnd		(_T("close_btn1"))
+#define BT_OK_FILISH			(_T("btn_ok1"))
+
+class CSignOutUI :
+	public WindowImplBase
+{
+public:
+	CSignOutUI();
+	~CSignOutUI();
+	void InitWindow();
+	virtual void OnFinalMessage(HWND hWnd);
+	virtual void Notify(TNotifyUI& msg);
+
+	DUI_DECLARE_MESSAGE_MAP();
+
+	void OnCloseWnd(TNotifyUI& msg);
+	void OnFilishMatch(TNotifyUI& msg);
+
+	void ShowMatchInfo();
+private:
+	int				m_nBmp;
+	CRegisterInfo*	m_RegInfo;
+protected:
+	virtual LPCTSTR GetWindowClassName() const;
+	virtual CDuiString GetSkinFolder();
+	virtual CDuiString GetSkinFile();
+};
+
