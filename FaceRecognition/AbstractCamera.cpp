96765a6a3ad4401a9bf715b3a5509b951667126b
kevin <janyboo@gmail.com>
2016-08-05 17:14:13 +0800

    增加IP摄像头和摄像头框架

---
+++ FaceRecognition/AbstractCamera.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "AbstractCamera.h"
+#include <Poco/Buffer.h>
+#include <Poco/NotificationCenter.h>
+#include "CaptureNotification.h"
+
+using Poco::NotificationCenter;
+using Poco::Buffer;
+
+#pragma warning(disable:4800)
