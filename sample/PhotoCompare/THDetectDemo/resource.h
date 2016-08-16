ea24356e7d2af2900208dff171dd53a81c2d7809
mk-z <zyf079@gmail.com>
2016-08-16 16:04:38 +0800

    添加灰度图人脸识别和图像压缩后人脸识别

--- sample/PhotoCompare/THDetectDemo/resource.h
+++ sample/PhotoCompare/THDetectDemo/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by THDetectDemo.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 THDetectDemo.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -21,6 +21,9 @@
 #define IDC_BROWSE3                     1007
 #define IDC_PERSON                      1008
 #define IDC_APPLY                       1009
+#define IDC_BUTTON1                     1010
+#define IDC_BUTTON2                     1011
+#define IDC_BUTTON3                     1012
 
 // Next default values for new objects
 // 
@@ -28,7 +31,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
