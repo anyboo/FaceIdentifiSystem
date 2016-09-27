1253ee600fae3d7992ca41015aef943a8678ddb1
wwang13 <191373193@qq.com>
2016-09-27 15:47:48 +0800

    1341

--- FaceRecognition/App.cpp
+++ FaceRecognition/App.cpp
@@ -27,28 +27,27 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*l
 	HRESULT Hr = ::CoInitialize(NULL);
 	if (FAILED(Hr)) return 0;
 
-//	Loggering::Logger_initiation();
+	Loggering::Logger_initiation();
 
-	//init face
-	//THFI_Param param;
-//	ValueSetting set;
-//	param.nMinFaceSize = std::stoi(set.GetFaceSize());
-	//param.nRollAngle = 145;
-	//param.bOnlyDetect = true;
-	//THFI_Create(1, &param);
+	THFI_Param param;
+	ValueSetting set;
+	param.nMinFaceSize = std::stoi(set.GetFaceSize());
+	param.nRollAngle = 145;
+	param.bOnlyDetect = true;
+	THFI_Create(1, &param);
 
-	//short ret = EF_Init(1);
-	//if (ret == 1)
-	//{
-	//	cout << "Feature init ok" << endl;
-	//}
+	short ret = EF_Init(1);
+	if (ret == 1)
+	{
+		cout << "Feature init ok" << endl;
+	}
 
 
 	//init database
 	QFileSqlite *pDb = QFileSqlite::getInstance();
 	pDb->createTable(CREATE_USER_INFO_TABLE);
 	
-//	RegUserInfo::init();
+	RegUserInfo::init();
 
 	CAppInitialize  initial;
 	
@@ -64,8 +63,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*l
 
 	CPaintManagerUI::MessageLoop();
 
-//	THFI_Release();
-//	EF_Release();
+	THFI_Release();
+	EF_Release();
 
 
 	::CoUninitialize();
